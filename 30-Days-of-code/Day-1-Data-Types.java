7f87bfc079300b20846f1f2f457b8d67ed1aa7fe
mahmoudsaeed99 <36303424+mahmoudsaeed99@users.noreply.github.com>
2020-08-12 17:25:01 +0200

    Create Day-1-Data-Types.java

---
+++ 30-Days-of-code/Day-1-Data-Types.java
@@ -0,0 +1,22 @@
+/* Declare second integer, double, and String variables. */
+        int i1 = scan.nextInt();
+        
+        double d1 = scan.nextDouble();
+        scan.nextLine();
+        String s1 =  scan.nextLine();
+        
+        
+
+        /* Read and save an integer, double, and String to your variables.*/
+        // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
+        System.out.println(i+i1);
+        System.out.println(d+d1);
+        System.out.println(s+s1);
+        
+
+        /* Print the sum of both integer variables on a new line. */
+
+        /* Print the sum of the double variables on a new line. */
+		
+        /* Concatenate and print the String variables on a new line; 
+        	the 's' variable above should be printed first. */
